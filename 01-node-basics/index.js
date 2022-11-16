4f4d4c64adba7059ca7f2368b28428724d6ff49d
Sumit K <synergy2411@outlook.com>
2022-11-16 15:29:55 +0530

    initial commit

---
+++ 01-node-basics/index.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+
+const content = fs.readFileSync("./test.txt")           // << Data transfer over streams using buffer
+
+console.log(content.toString());
+
+fs.writeFileSync("./test3.txt", content)
+
+
+// const path = require("path")
+
+// const url = "http://www.example.com/public/index.html";
+
+// console.log("File name", path.basename(url));
+// console.log("Directory name", path.dirname(url));
+// console.log("Extension name", path.extname(url));
+
+
+
+
+// const os = require("os")
+
+// console.log("Total Memory : ", os.totalmem());
+// console.log("Free Memory : ", os.freemem());
+
+// console.log("Number of CPU's  : ", os.cpus().length);
+
+// console.log("Architecture : ", os.arch());
+
+
+
+
+
+
+
+
+
+
+
+// const colors = require("colors")
+// const yargs = require("yargs")
+
+// // print
+// yargs.command({
+//     command: "print",
+//     description: "to print something",
+//     builder: {
+//         text: {
+//             type: "string",
+//             demandOption: true
+//         }
+//     },
+//     handler: function (args) {
+//         console.log("PRINTING : ", args.value)
+//     }
+// })
+
+// // read
+// yargs.command({
+//     command: "read",
+//     description: "to read the content",
+//     builder: {
+//         title: {
+//             type: "string",
+//             demandOption: true
+//         }
+//     },
+//     handler: function (args) {
+//         console.log("TITLE : ", args.title);
+//     }
+// })
+
+
+// yargs.parse()
+
+// // console.log("HEllo World".red);
+// // console.log("HEllo World".green);
+// // console.log("HEllo World".inverse);
+// // console.log("HEllo World".rainbow);
---
+++ ref.md
@@ -0,0 +1,27 @@
+# NodeJS Installer -
+
+> Node Package Manager (NPM)
+> Node Runtime Environment (NRE)
+> Node Native/Core Modules
+
+# NodeJS Follows
+
+> CommonJS Module System (default)
+
+    - module.exports
+    - require()
+
+> ES Module System (.mjs)
+
+    - import
+    - export
+
+# NPM Commands
+
+> npm init - generates package.json file in project folder
+> npm install chalk
+> npm install yargs
+
+> node index.js --help
+> node index.js print --help
+> node index.js print --value="any value"
