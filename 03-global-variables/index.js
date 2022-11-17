153ad9b3d458ed6f1751053ef4d2fe622c293bec
Sumit K <synergy2411@outlook.com>
2022-11-17 12:38:33 +0530

    semver

---
+++ 03-global-variables/index.js
@@ -0,0 +1,19 @@
+// Timers
+
+// setTimeout(() => {
+//     console.log("3 Seconds left");
+// }, 3000)
+
+// let count = 0;
+
+// setInterval(() => {
+//     ++count;
+//     console.log(count);
+// }, 1000)
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+// console.log(process.argv);               // <<< tons of properties about the nodejs process
